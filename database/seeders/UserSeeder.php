9edc8051732d6e9d492c468af2496ac85c324cd0
Ozodbek4252 <ozodbekmcfly@gmail.com>
2023-09-06 10:14:12 +0500

    Multi Auth Dashboard

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -16,8 +15,35 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name' => 'Ozodbek',
+            'username' => 'adminuser',
+            // 'image' => '',
+            'phone' => '+998944424252',
             'email' => 'ozodbek1111@gmail.com',
+            'role' => 'admin',
+            'status' => 'active',
             'password' => Hash::make('ozodbek1111')
+        ]);
+
+        User::create([
+            'name' => 'Vendor',
+            'username' => 'vendoruser',
+            // 'image' => '',
+            'phone' => '+998944424252',
+            'email' => 'vendor@gmail.com',
+            'role' => 'vendor',
+            'status' => 'active',
+            'password' => Hash::make('password')
+        ]);
+ 
+        User::create([
+            'name' => 'User',
+            'username' => 'user',
+            // 'image' => '',
+            'phone' => '+998944424252',
+            'email' => 'user@gmail.com',
+            'role' => 'user',
+            'status' => 'active',
+            'password' => Hash::make('password')
         ]);
     }
 }
