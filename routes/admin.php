d5efc97218fe2ce174a55ff341e30903ecbd6601
Ozodbek4252 <ozodbekmcfly@gmail.com>
2023-09-13 20:15:49 +0500

    Back: Category model/migration/Controller/DataTable

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
 
@@ -18,3 +19,9 @@ Route::post('profile/update/password', [ProfileController::class, 'updatePasswor
 
 /** Slider Routes */
 Route::resource('slider', SliderController::class);
+
+/** Category Routes */
+Route::resource('category', CategoryController::class);
+
+
+
