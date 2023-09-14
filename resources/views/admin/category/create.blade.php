1697c067e48bb5529e9e9d0fa08052694c216575
Ozodbek4252 <ozodbekmcfly@gmail.com>
2023-09-14 21:35:26 +0500

    Back: Category Create page

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.layouts.master')
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Category</h1>
+        </div>
+
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Create Category</h4>
+
+                        </div>
+                        <div class="card-body">
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="icon">Icon</label>
+                                    <div>
+                                        <button class="btnt btn-primary" data-selected-class="btn-danger"
+                                        role="iconpicker" data-unselected-class="btn-info"></button>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input type="text" name="name" id="name" class="form-control" value="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="status">Status</label>
+                                    <select name="status" id="status" class="form-control">
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Create</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
