3d236ee526114257490bd4eafff030dddfa87e25
Ozodbek4252 <ozodbekmcfly@gmail.com>
2023-09-14 21:27:10 +0500

    bootstrap icon picker fixed

--- resources/views/admin/layouts/scripts.blade.php
+++ resources/views/admin/layouts/scripts.blade.php
@@ -18,7 +18,7 @@
 <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="{{ asset('backend/assets/js/bootstrap-iconpicker.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/bootstrap-iconpicker.bundle.min.js') }}"></script>
 
 <!-- Page Specific JS File -->
 <script src="{{ asset('backend/assets/js/page/index-0.js') }}"></script>
