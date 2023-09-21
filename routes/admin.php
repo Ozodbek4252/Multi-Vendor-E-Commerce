e9a1a0c68c47a18b95578bfb3f92c23e5811d8e8
Ozodbek4252 <ozodbekmcfly@gmail.com>
2023-09-21 21:59:19 +0500

    SubCategory created

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
+use App\Http\Controllers\Backend\SubCategoryController;
 
 /** Admin Routes */
 
@@ -21,8 +22,9 @@ Route::post('profile/update/password', [ProfileController::class, 'updatePasswor
 Route::resource('slider', SliderController::class);
 
 /** Category Routes */
-Route::put('change-status', [CategoryController::class, 'changeStatus'])->name('category.change-status');
+Route::put('category/change-status', [CategoryController::class, 'changeStatus'])->name('category.change-status');
 Route::resource('category', CategoryController::class);
 
-
-
+/** Sub Category Routes */
+Route::put('sub-category/change-status', [SubCategoryController::class, 'changeStatus'])->name('sub-category.change-status');
+Route::resource('sub-category', SubCategoryController::class);
