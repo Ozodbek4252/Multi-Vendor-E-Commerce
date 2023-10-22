19c79db346a7804526858d13a855ea1520b0c602
Ozodbek4252 <ozodbekmcfly@gmail.com>
2023-10-22 22:12:24 +0500

    Admin: Product: Variant: show only related variants to specific product

--- app/DataTables/ProductVariantDataTable.php
+++ app/DataTables/ProductVariantDataTable.php
@@ -62,7 +62,7 @@ class ProductVariantDataTable extends DataTable
      */
     public function query(ProductVariant $model): QueryBuilder
     {
-        return $model->newQuery();
+        return $model->where('product_id', request()->product_id)->newQuery();
     }
 
     /**
